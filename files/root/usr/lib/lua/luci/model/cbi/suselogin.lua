8192dbed7b25f569ab8b1e6cbee18a28315179e3
sj8199 <730877073@qq.com>
2024-12-19 20:51:59 +0800

    Update suselogin.lua

--- files/root/usr/lib/lua/luci/model/cbi/suselogin.lua
+++ files/root/usr/lib/lua/luci/model/cbi/suselogin.lua
@@ -20,10 +20,9 @@ pass.password = true
 pass.rmempty = false
 
 isp = s:option(ListValue, "isp", translate("运营商"))
-isp:value("%E5%AE%9C%E5%AE%BE%E7%94%B5%E4%BF%A1", translate("宜宾电信互联网"))
-isp:value("%E5%AE%9C%E5%AE%BE%E7%A7%BB%E5%8A%A8", translate("宜宾移动互联网"))
 isp:value("%E6%A0%A1%E5%9B%AD%E7%BD%91", translate("校园网"))
-isp:value("%E5%AE%9C%E5%AE%BE%E8%81%94%E9%80%9A", translate("宜宾联通互联网"))
+isp:value("%E8%87%AA%E8%B4%A1%E7%A7%BB%E5%8A%A8", translate("自贡移动互联网"))
+isp:value("%E8%87%AA%E8%B4%A1%E8%81%94%E9%80%9A", translate("自贡联通互联网"))
 
 interval = s:option(Value, "interval", translate("间隔时间"), translate("每隔多少时间(≥1)检测一下网络是否连接正常，如果网络异常则会尝试连接(单位:分钟)"))
 interval.default = 5
@@ -33,7 +32,7 @@ auto_offline = s:option(Flag, "auto_offline", translate("自动下线"), transla
 auto_offline.rmempty = false
 
 success = s:option(DummyValue,"opennewwindow",translate("认证页面"))
-success.description = translate("<input type=\"button\" class=\"cbi-button cbi-button-save\" value=\"打开认证页\" onclick=\"window.open('http://10.23.2.4/eportal/success.jsp')\" /><input type=\"button\" class=\"cbi-button cbi-button-save\" value=\"打开自助服务\" onclick=\"window.open('http://10.23.2.6:8080/selfservice')\" /><br />可查看认证状态和管理在线设备")
+success.description = translate("<input type=\"button\" class=\"cbi-button cbi-button-save\" value=\"打开认证页\" onclick=\"window.open('http://10.60.208.4')\" /><input type=\"button\" class=\"cbi-button cbi-button-save\" value=\"打开自助服务\" onclick=\"window.open('http://10.60.224.200:8080/selfservice')\" /><br />可查看认证状态和管理在线设备")
 
 
 local apply = luci.http.formvalue("cbi.apply")
